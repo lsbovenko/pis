1962f0fbba9228013d61c3017b70a1fcf8a82eed
Valera Kostukevich <valery@ikantam.com>
2019-01-17 15:37:00 +0300

    sending emails about comments to ideas

---
+++ app/Mail/AbstractComment.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: valeriy
+ * Date: 17.01.19
+ * Time: 16:44
+ */
+
+namespace App\Mail;
+
+
+use App\Models\Comment;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class AbstractComment extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * @var $comment
+     */
+    protected $comment;
+
+    /**
+     * ToAll constructor.
+     * @param Comment $comment
+     */
+    public function __construct(Comment $comment)
+    {
+        $this->comment = $comment;
+    }
+}
---
+++ app/Mail/IdeaComment/ToAll.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: valeriy
+ * Date: 17.01.19
+ * Time: 13:32
+ */
+
+namespace App\Mail\IdeaComment;
+
+
+use App\Mail\AbstractComment;
+
+class ToAll extends AbstractComment
+{
+    /**
+     * Build the message.
+     *
+     * @return ToAll
+     */
+    public function build()
+    {
+        return $this
+            ->subject('Добавлен новый комментарий в PIS')
+            ->view('emails.comments.to-all', [
+                'comment' => $this->comment
+            ]);
+    }
+}
